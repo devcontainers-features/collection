451f058b12b4db3594237f30585e56d69d11d734
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-11-24 12:33:42 +0400

    feat: add postgresql-client devcontainer feature
    - Add new postgresql-client feature with installation script and tests
    - Simplify atlas feature name and remove documentation URL
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

---
+++ src/postgresql-client/README.md
@@ -0,0 +1,15 @@
+# Atlas
+
+PostgreSQL Client is the PostgreSQL interactive terminal.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers-features/collection/postgresql-client:1": {}
+}
+```
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/devcontainers/feature-starter/blob/main/src/color/devcontainer-feature.json). Add additional notes to a `NOTES.md`._
---
+++ test/postgresql-client/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Provides the 'check' and 'reportResults' commands.
+# shellcheck disable=SC1091
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
+check "execute command" bash -c "psql --help | grep 'psql is the PostgreSQL interactive terminal.'"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
